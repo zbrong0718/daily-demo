1ca776c6b13b0eba753635016e8a5f59397613e5
Jessons Wang <zhouborong0718@qq.com>
2019-05-23 01:17:00 +0800

    fix syntax error in JSON.js 2

--- JSON.js
+++ JSON.js
@@ -74,7 +74,7 @@ function parse(json) {
   var rx_one = /^[\],:{}\s]*$/; var rx_two = /\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g;
   var rx_three = /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g;
   var rx_four = /(?:^|:|,)(?:\s*\[)+/g;
-  if ( rx_one.test( json.replace(rx_two, "@").replace(rx_three, "]").replace(rx_four, "") ) {
+  if ( rx_one.test( json.replace(rx_two, "@").replace(rx_three, "]").replace(rx_four, "") ) ) {
     var obj = eval( "(" +json + ")" );
   }
   return obj;
